9d6631b64e18e473537f176878e0d86a4cb1366b
Rr <raymund.bermudes@gdslinkasia.com>
2025-11-13 06:53:57 +0000

    fix

--- src/data/constants.js
+++
@@ -1,5 +0,0 @@
-export const STATUSES = {
-    wantToRead: { id: "wantToRead", label: "Want to Read" },
-    reading: { id: "reading", label: "Reading" },
-    finished: { id: "finished", label: "Finished" },
-  };
